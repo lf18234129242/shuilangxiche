73f88e4d372f0061bbbdf7dbc3d9a7be1edd9e71
刘员外 <lf18234129242>
2019-03-21 19:40:58 +0800

    '完成登陆和个人中心页面'

---
+++ src/views/PersonalInformation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="personalInformation">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: 'value'
+            }
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+.personalInformation{
+    width: 100%;
+    height: 100vh;
+    background: #000;
+}
+</style>
